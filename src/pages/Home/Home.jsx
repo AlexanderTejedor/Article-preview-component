04f5b41f6f08027734cc5200f5ffc0691c60ad1e
Alexander Tejedor <atromero.98@gmail.com>
2025-05-15 14:05:16 -0500

    feat(share): Implement logic to toggle interface on share button click

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,31 @@
+import { useState } from 'react'
 import bgImg from '../../assets/img/drawers.jpg'
 import avatarImg from '../../assets/img/avatar-michelle.jpg'
 import shareIcon from '../../assets/img/icon-share.svg'
+import facebookIcon from '../../assets/img/icon-facebook.svg'
+import twitterIcon from '../../assets/img/icon-twitter.svg'
+import pinterestIcon from '../../assets/img/icon-pinterest.svg'
 
 const Home = () => {
+    const [shared, setShared] = useState(false);
+
+    const handleClick = () => {
+        setShared(!shared);
+    }
+
     return (
         <main className='h-screen flex items-center justify-center bg-light-grayish-blue'>
             <section className='bg-white max-w-83 rounded-xl'>
                 <figure className='h-51 overflow-hidden relative rounded-t-xl mb-9'>
                     <img className='absolute -top-4' src={ bgImg } alt="A set of wooden drawers with decorative items on top" />
                 </figure>
-                <section className='px-10'>
-                    <div>
+                <section>
+                    <div className='px-10'>
                         <h3 className='text-very-dark-grayish-blue font-semibold text-base mb-3'>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h3>
-                        <p className='text-[13px] mb-7 text-desaturated-dark-blue'>Ever been in a room and felt like something was missing?
+                        <p className='text-[13px] text-desaturated-dark-blue'>Ever been in a room and felt like something was missing?
                         Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.</p>
                     </div>
-                    <div className='flex justify-between items-center mb-5'>
+                    <div className={`${shared ? 'hidden' : 'flex justify-between items-center mb-5 px-10 mt-7'}`}>
                         <div className='flex gap-3 justify-center items-center'>
                             <img className='size-10 rounded-full' src={avatarImg} alt="Portrait of Michelle, the author of the article" />
                             <div>
@@ -23,7 +33,20 @@ const Home = () => {
                                 <p className='text-xs text-grayish-blue'>28 Jun 2020</p>
                             </div>
                         </div>
-                        <img className='bg-light-grayish-blue p-3 rounded-full' src={shareIcon} alt="Share Icon" />
+                        <img className='bg-light-grayish-blue p-3 rounded-full cursor-pointer' src={shareIcon} alt="Share Icon" onClick={handleClick}/>
+                    </div>
+                    <div className={`${shared ? 'bg-very-dark-grayish-blue px-10 py-4 flex justify-between items-center rounded-b-lg mt-4' : 'hidden'}`}>
+                        <div className='flex gap-4'>
+                            <p className='text-grayish-blue text-sm tracking-[4.5px]'>SHARE</p>
+                            <div className='flex gap-4'>
+                                <img src={facebookIcon} alt="Facebook Icon" />
+                                <img src={twitterIcon} alt="Twitter Icon" />
+                                <img src={pinterestIcon} alt="Pinterest Icon" />
+                            </div>
+                        </div>
+                        <div className=' bg-desaturated-dark-blue rounded-full w-9 h-9 flex items-center justify-center'>
+                            <i className="ri-share-forward-fill text-white text-xl"></i>
+                        </div>
                     </div>
                 </section>
             </section>
